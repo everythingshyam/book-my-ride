566455359a5bd6d44c53e10407bcc6454b520477
Shyam <shyampersonal2000@gmail.com>
2021-12-24 16:50:43 +0530

    cleared up import export functions
    and tested some of the functions in Support Class

--- CoreProgram.cpp
+++ CoreProgram.cpp
@@ -136,144 +136,63 @@ public:
         time_t mytime;
         mytime = time(NULL);
         printf(ctime(&mytime));
-        printf("\n\t\t\t***Welcome to %s Hospital***\n");
-
+        cout << "\n------------------------------***Book My Ride***--------------------------------";
         printf("\n--------------------------------------------------------------------------------");
     }
 
-    void encrypt(char *S)
+    void encrypt(string *s)
     {
         /*
 		Logic for encryption
 		
-		Basic rules applied, but effective enough
-
-		FOR STRING
-		if alphabet, first, case is changed then, considering the alphabets forming a circle, the opposite letter is chosen
-		if digit, again, opposite digit is chosen, for ex, 0 replaced by 9, 1 replaced by 8
-		Other symbols kept as it is
-		For Decryption, just reverse the processes
+		to be thought yet
 	*/
-        int l = strlen(S);
-        for (int i = 0; i < l; i++)
-        {
-            if (S[i] >= 65 && S[i] <= 90) //UPPER CASE LETTERS
-            {
-                S[i] += 32;
-                S[i] = 122 - (S[i] - 97);
-            }
-            else if (S[i] >= 97 && S[i] <= 122) //LOWER CASE LETTERS
-            {
-                S[i] -= 32;
-                S[i] = 90 - (S[i] - 65);
-            }
-            else if (S[i] >= 48 && S[i] <= 57) //DIGITS
-            {
-                S[i] = 57 - (S[i] - 48);
-            }
-        }
     }
     ////////////////////////////////////////////////////////////////////////////////
-    void decrypt(char *S)
+    void decrypt(string *s)
     {
-        int l = strlen(S);
-        for (int i = 0; i < l; i++)
-        {
-            if (S[i] >= 65 && S[i] <= 90) //UPPER CASE LETTERS
-            {
-                S[i] += 32;
-                S[i] = 122 - (S[i] - 97);
-            }
-            else if (S[i] >= 97 && S[i] <= 122) //LOWER CASE LETTERS
-            {
-                S[i] -= 32;
-                S[i] = 90 - (S[i] - 65);
-            }
-            else if (S[i] >= 48 && S[i] <= 57) //DIGITS
-            {
-                S[i] = 57 - (S[i] - 48);
-            }
-        }
     }
     ////////////////////////////////////////////////////////////////////////////////
     int encryptInt(int a)
     {
-        int b = 0;
-
-        while (a != 0)
-        {
-            b = b * 10 + (9 - a % 10);
-            a /= 10;
-        }
-
-        a = 0; //for confirmation
-
-        while (b != 0)
-        {
-            a = a * 10 + b % 10;
-            b /= 10;
-        }
-
         return a;
     }
     ////////////////////////////////////////////////////////////////////////////////
     int decryptInt(int a)
     {
-        int b = 0;
-
-        while (a != 0)
-        {
-            b = b * 10 + (9 - a % 10);
-            a /= 10;
-        }
-
-        a = 0; //for confirmation
-
-        while (b != 0)
-        {
-            a = a * 10 + b % 10;
-            b /= 10;
-        }
-
         return a;
     }
 
     void import()
     {
-        int dataCount; //temp line
-        //importing from PatientRecords1024.txt
-        FILE *fp = fopen("FileName.txt", "r");
+        //importing from UserTaxi.txt
+        FILE *fp = fopen("UserTaxi.txt", "r");
         if (fp == NULL)
         {
-            printf("\nFile Not Found!");
-            fp = fopen("FileName.txt", "w");
-            printf("\nNew File has been created.");
+            printf("\nTaxi Users Database Not Found!");
+            fp = fopen("UserTaxi.txt", "w");
+            printf("\nNew Taxi Users Database has been created.");
             fclose(fp);
         }
-        dataCount = fread(userTaxi, sizeof(struct UserTaxi), 100, fp);
-        printf("\nFile imported successfully.");
+        userTaxiCount = fread(userTaxi, sizeof(struct UserTaxi), maxUserCount, fp);
+        printf("\nTaxi Users Database imported successfully.");
         fclose(fp);
 
         //Decrypting all data
-
-        //DECRYPTION COMPLETED
     }
     void backup()
     {
-        int dataCount; //temp line
         //Encrypting all data
 
-        //ENCRYPTION COMPLETED
-
-        //updating FileName.txt
-        FILE *fp = fopen("FileName.txt", "w");
+        //updating UserTaxi.txt
+        FILE *fp = fopen("UserTaxi.txt", "w");
         if (fp == NULL)
         {
-            printf("/nBackup File is empty!");
+            printf("/nTaxi Users Database is empty!");
             exit(1);
         }
-        fwrite(userTaxi, sizeof(struct UserTaxi), dataCount, fp);
-        printf("\nBackup File updated successfully.");
+        fwrite(userTaxi, sizeof(struct UserTaxi), userTaxiCount, fp);
+        printf("\nTaxi Users Database updated successfully.");
         fclose(fp);
     }
 };
@@ -342,10 +261,12 @@ public:
 int main()
 {
     //Main Menu here first, will be like switch inside switch
-    cout << "hello there...";
     Support S;
     S.wait(5);
     cout << "\nHello again";
+    S.screenReset();
+    S.import();
+    S.backup();
     return 0;
 }
 
